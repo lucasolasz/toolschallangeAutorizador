1ce7c5e8f1c6b32256c76e0a4b6a0ba05ec8437c
lucasolasz <olaszlucas072@gmail.com>
2026-03-06 16:12:55 -0300

    feat: Inclui uteis para conversao de objeto

--- src/main/java/com/arphoenix/toolschallangeAutorizador/util/ObjetoResponseUtil.java
+++ src/main/java/com/arphoenix/toolschallangeAutorizador/util/ObjetoResponseUtil.java
@@ -3,6 +3,10 @@ package com.arphoenix.toolschallangeAutorizador.util;
 import java.util.Random;
 import java.util.UUID;
 
+import com.arphoenix.toolschallangeAutorizador.domain.enums.StatusTransacao;
+import com.arphoenix.toolschallangeAutorizador.domain.records.PagamentoRequestRecord;
+import com.arphoenix.toolschallangeAutorizador.domain.records.PagamentoResponseRecord;
+
 public class ObjetoResponseUtil {
 
     public static String gerarNSU() {
@@ -19,5 +23,60 @@ public class ObjetoResponseUtil {
         if (cartao == null || cartao.length() < 11)
             return cartao;
         return "**** **** **** " + cartao.substring(cartao.length() - 4);
+    }
+
+    public static PagamentoResponseRecord criarRespostaAutorizada(PagamentoRequestRecord request) {
+
+        var input = request.transacao();
+
+        String nsu = ObjetoResponseUtil.gerarNSU();
+        String codAutorizacao = ObjetoResponseUtil.gerarCodigoAutorizacao();
+        StatusTransacao status = StatusTransacao.AUTORIZADO;
+
+        var descricaoOutput = new PagamentoResponseRecord.DescricaoOutput(
+                input.descricao().valor(),
+                input.descricao().dataHora(),
+                input.descricao().estabelecimento(),
+                nsu,
+                codAutorizacao,
+                status);
+
+        var formaPagamentoOutput = new PagamentoResponseRecord.FormaPagamentoOutput(
+                input.formaPagamento().tipo(),
+                input.formaPagamento().parcelas());
+
+        var transacaoOutput = new PagamentoResponseRecord.TransacaoOutput(
+                input.id(),
+                input.cartao(),
+                descricaoOutput,
+                formaPagamentoOutput);
+
+        return new PagamentoResponseRecord(transacaoOutput);
+    }
+
+    public static PagamentoResponseRecord criarRespostaRecusada(PagamentoRequestRecord request) {
+
+        var input = request.transacao();
+
+        var descricaoOutput = new PagamentoResponseRecord.DescricaoOutput(
+                input.descricao().valor(),
+                input.descricao().dataHora(),
+                input.descricao().estabelecimento(),
+                null, // nsu nulo ou vazio
+                null, // código autorização nulo
+                StatusTransacao.NEGADO // ou outro status que você preferir
+        );
+
+        var formaPagamentoOutput = new PagamentoResponseRecord.FormaPagamentoOutput(
+                input.formaPagamento().tipo(),
+                input.formaPagamento().parcelas());
+
+        var transacaoOutput = new PagamentoResponseRecord.TransacaoOutput(
+                input.id(),
+                input.cartao(),
+                descricaoOutput,
+                formaPagamentoOutput);
+
+        return new PagamentoResponseRecord(transacaoOutput);
     }
 }
