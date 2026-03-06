50ba23dc13e0c5a3942489778ce542cf3ece365c
lucasolasz <olaszlucas072@gmail.com>
2026-03-06 16:12:28 -0300

    chore: Inclui o producer

--- src/main/java/com/arphoenix/toolschallangeAutorizador/messaging/producer/PagamentoProducer.java
+++ src/main/java/com/arphoenix/toolschallangeAutorizador/messaging/producer/PagamentoProducer.java
@@ -3,19 +3,18 @@ package com.arphoenix.toolschallangeAutorizador.messaging.producer;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
-import com.arphoenix.toolschallangeAutorizador.domain.records.PagamentoRequestRecord;
+import com.arphoenix.toolschallangeAutorizador.domain.records.PagamentoResponseRecord;
+
+import lombok.AllArgsConstructor;
 
 @Service
+@AllArgsConstructor
 public class PagamentoProducer {
 
-    private final KafkaTemplate<String, PagamentoRequestRecord> kafkaTemplate;
+    private final KafkaTemplate<String, PagamentoResponseRecord> kafkaTemplate;
 
-    public PagamentoProducer(KafkaTemplate<String, PagamentoRequestRecord> kafkaTemplate) {
-        this.kafkaTemplate = kafkaTemplate;
-    }
-
-    public void enviarPagamentoParaTopico(PagamentoRequestRecord pagamento) {
-        kafkaTemplate.send("vendas-pendentes", pagamento);
+    public void enviarPagamentoParaTopico(PagamentoResponseRecord pagamentoResponse) {
+        kafkaTemplate.send("vendas-finalizadas", pagamentoResponse);
     }
 
 }
